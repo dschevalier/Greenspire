bd6da993d4d7371945793cd6ae00121926663341
David Chevalier <dschevalier03@gmail.com>
2018-10-22 16:56:42 -0400

    Updates...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,25 @@
+// BUTTON POPUP TEXT
 $(function () {
     $('[data-toggle="popover"]').popover()
 })
-
 $('.popover-dismiss').popover({
     trigger: 'focus'
 })
 
+// ADDS SMOOTH SCROLLING TO NAVBAR LINKS
+$("a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1000, function () {
+            window.location.hash = hash;
+        });
+    }
+});
+
+// COLLAPSE MOBILE NAVIGATION AFTER CLICK
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
