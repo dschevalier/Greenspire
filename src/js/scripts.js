9287df365d3c0c42ef90accb75d1de88eae2cb20
David Chevalier <dschevalier03@gmail.com>
2018-10-22 20:17:43 -0400

    Updates...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,7 +13,7 @@ $("a").on('click', function (event) {
         var hash = this.hash;
         $('html, body').animate({
             scrollTop: $(hash).offset().top
-        }, 1000, function () {
+        }, 800, function () {
             window.location.hash = hash;
         });
     }
